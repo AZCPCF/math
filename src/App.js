4d4ed0e0e97041e771f4c4027e39bb231350ff33
Alisan <99.a.cpcf.r.99@gmail.com>
2024-11-11 00:02:39 +0330

    add labels for a,b & c

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import "./App.css";
 import useQe from "./hooks/useQe";
 function App() {
@@ -8,20 +9,23 @@ function App() {
       <header className="App-header">
         <h1 className="text-3xl m-4 text-green-400">Quadratic Equation</h1>
         {Object.keys(qe).map((item) => (
-          <input
-            className="text-black w-[90%] md:w-1/3 sm:w-1/2 xl:w-1/5 border-[3px] border-green-400 rounded-lg m-2 p-2"
-            placeholder={item}
-            id={item}
-            key={item}
-            onChange={handleChnageQe}
-            value={qe[item]}
-          />
+          <div className="w-[90%] md:w-1/3 sm:w-1/2 xl:w-1/5 text-left m-2">
+            <label className="p-2" htmlFor={item}>{item}</label>
+            <input
+              className="text-black w-full border-[3px] border-green-400 rounded-lg p-2 mt-2"
+              placeholder={item}
+              id={item}
+              key={item}
+              onChange={handleChnageQe}
+              value={qe[item]}
+            />
+          </div>
         ))}
 
         <input
           className="border-2 w-[90%] md:w-1/3 sm:w-1/2 xl:w-1/5 border-green-400 rounded-lg m-2 p-2 hover:bg-green-400"
           type="submit"
-          value={'submit'}
+          value={"submit"}
           onClick={solveQe}
         />
         <p className="text-green-400 mt-14">{result}</p>
