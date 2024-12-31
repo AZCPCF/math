42b1cd1bed606b55e658903f9972f8d4833bc9dd
Alisan <99.a.cpcf.r.99@gmail.com>
2025-01-01 02:41:36 +0330

    fix Title component issue
    use children instead title props

--- src/components/title/index.jsx
+++ src/components/title/index.jsx
@@ -1,4 +1,4 @@
-const Title = ({ className, title }) => (
-  <h1 className={`text-3xl m-4 text-green-400 ${className}`}>{title}</h1>
+const Title = ({ className, children }) => (
+  <h1 className={`text-3xl m-4 text-green-400 ${className}`}>{children}</h1>
 );
 export default Title
